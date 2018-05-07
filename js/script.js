2610c98df7473b69c02d433e383e06c44958e87e
Geoff Groenendale <g.groenendale@wsu.edu>
2018-05-07 16:19:53 -0700

    Add initial files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Title: Geoff Groenendale - Portfolio
+ * Author: Geoff Groenendale
+ * Date: 5/7/2018
+ * Description: This is a javascript file for primary
+ * functions for the portfolio website.
+ */
+
+window.onload = setup();
+
+function setup() {
+	alert('This works.');
+}

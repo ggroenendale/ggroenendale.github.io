a4de85b14728ab1b2f35c27407c998d83b17e03c
Geoff Groenendale <g.groenendale@wsu.edu>
2018-05-08 14:55:33 -0700

    Updated files

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
  * Author: Geoff Groenendale
  * Date: 5/7/2018
  * Description: This is a javascript file for primary
- * functions for the portfolio website.
+ * functions for the portfolio website. These functions
+ * are unique to the portfolio site but this file references
+ * the workhorse.js file that has several clerical functions.
  */
 
 window.onload = function() {
@@ -11,9 +13,25 @@ window.onload = function() {
 }
 
 window.onresize = function() {
-	
+
 }
 
 function setup() {
+	load_workhorse();
+	change_page_name();
+}
 
+function load_workhorse() {
+	let tag = document.createElement('script');
+	tag.src = 'workhorse.js';
+	document.getElementById('scripts').appendChild(tag);
+}
+
+function change_page_name() {
+	let cur_class = document.getElementByTagName('html').className
+	let cur_name = cur_class.substring(search(cur_class.search("_")))
+	cur_name = first_l_UP(cur_name);
+	console.log(cur_name);
+	let page_name = document.getElementById('cur-page');
+	page_name.innerText = cur_name;
 }
---
+++ js/workhorse.js
@@ -0,0 +1,13 @@
+/**
+ * Title: Workhorse.js
+ * Author: Geoff Groenendale
+ * Date: 5/8/2018
+ * Description: This is a javascript file for primary
+ * functions for the portfolio website. These functions
+ * are unique to the portfolio site but this file references
+ * the workhorse.js file that has several clerical functions.
+ */
+
+function first_l_UP(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}

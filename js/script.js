28983cf87d5fe6c53e602618c4fc7fe97469df36
Geoff Groenendale <g.groenendale@wsu.edu>
2018-05-07 22:56:17 -0700

    Updated files

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@
  * functions for the portfolio website.
  */
 
-window.onload = setup();
+window.onload = function() {
+	setup();
+}
+
+window.onresize = function() {
+	let side 	 = document.getElementById('sidebar');
+	let width 	 = side.clientWidth;
+	let gr_rect  = document.getElementById('gr-rect');
+	let rect_wid = width * 0.85;
+	gr_rect.style.width = rect_wid;
+}
 
 function setup() {
-	
+
 }

e5a5fac8f1353a36199400e84011957e3c35f4c0
Geoff Groenendale <g.groenendale@wsu.edu>
2018-05-07 23:56:01 -0700

    Updated files

--- js/script.js
+++ js/script.js
@@ -11,14 +11,7 @@ window.onload = function() {
 }
 
 window.onresize = function() {
-	// let side 	 = document.getElementById('sidebar');
-	// let width 	 = side.clientWidth;
-	// let gr_rect  = document.getElementById('gr-rect');
-	// let rect_wid = Math.floor(width * 0.85);
-	// let rect_lf	 = Math.floor(width * 0.15);
-	// gr_rect.setAttribute("style","width:" + rect_wid + "px");
-	// gr_rect.setAttribute("style","left:" + rect_lf + "px");
-	// console.log('New width ' + rect_wid);
+	
 }
 
 function setup() {
